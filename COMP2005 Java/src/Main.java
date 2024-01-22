b3be82556847b0165e76c8050fb6ebc5d1785b11
BungeeDragon <skillmanesther@gmail.com>
2024-01-22 13:01:53 +0000

    Simple calculation function added

---
+++ COMP2005 Java/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        calculation();
+
+
+    }
+
+    private static void calculation() {
+        int num1 = 5;
+        int num2 = 10;
+
+        int num3 = num1 * num2;
+        System.out.println(num3);
+    }
+}
