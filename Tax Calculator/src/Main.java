492ff79401141c75620d9ec87e7499a23f2e524c
Eskillman <skillmanesther@gmail.com>
2024-02-13 17:46:44 +0000

    fixed int to double

--- Tax Calculator/src/Main.java
+++ Tax Calculator/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static double taxThreshold20p = 12570;
     public static double taxThreshold40p = 50270;
-    public static int taxThreshold45p = 125140;
+    public static double taxThreshold45p = 125140;
 
     public static double taxBand20p = (taxThreshold40p - taxThreshold20p) * 0.8 + taxThreshold20p;
     public static double taxBand40p = (taxThreshold45p - taxThreshold40p) * 0.6 + taxBand20p;
@@ -33,12 +33,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        double income = 30000;
+        double income = 150000;
         double result = taxCalculator(income);
         double incomeTax = income - result;
 
         // System.out.println("Taxed amount of 40% between 50270 and 125140 " + taxBand40p);
-        // System.out.println("Total tax " + incomeTax);
+        System.out.println("Total tax " + incomeTax);
         System.out.println("Result after income tax reductions " + result);
 
     }
