c99a3ea4d28bcfc24339798200b7d3201f419716
(s) Esther Skillman <eskillman@socnet.soc.plymouth.ac.uk>
2024-03-05 16:22:58 +0000

    Initialised snakes_and_ladders_test with hashMap variable created in main

---
+++ Snakes_and_Ladders_Test/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+
+
+
+
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, String> board = new HashMap<String, String>();
+
+        // Add keys and values (tile num, value)
+        board.put(" ", "");
+        System.out.println(board);
+
+    }
+}
